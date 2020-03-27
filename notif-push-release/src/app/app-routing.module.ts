fffe9676cd403c5773268cc9bec594b4895d92e8
Ayoub KHOUYA <a.khouya.ing@gmail.com>
2020-03-27 01:02:28 +0100

    release

--- notif-push-release/src/app/app-routing.module.ts
+++ notif-push-release/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '', component: LoginComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
