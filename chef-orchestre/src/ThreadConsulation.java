6f4d835791ae5cbb9c77a8f5f3fe3493184cfb9d
Ayoub KHOUYA <a.khouya.ing@gmail.com>
2020-04-30 16:50:32 +0200

    add Chef-Orchestr repository

---
+++ chef-orchestre/src/ThreadConsulation.java
@@ -0,0 +1,58 @@
+
+import java.sql.*;
+import java.time.LocalDate;
+
+public class ThreadConsulation extends Thread {
+    /**
+     * @description: le thread qui consulte la base de données pour traiter les notifications crées.
+     *              => ajouter les notification vers la table envoie.
+     * */
+
+    private static final boolean running = true;
+
+    @Override
+    public void run() {
+        while (ThreadConsulation.running) {
+            Thread.onSpinWait();
+            try {
+                try {
+                    Connection connection = SettingsSystem.getConnection();
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery(SettingsSystem.sqlSelectNotifications);
+                    while (resultSet.next()) {
+                        envoieNotifications((id, cible, date) -> {
+                                    try {
+                                        Connection conn = SettingsSystem.getConnection();
+                                        String sql = "INSERT INTO mb_envoie (men_iduser, men_idnotification, mav_datecreat) VALUES (\"" + cible + "\", \""+ id + "\", '" + date + "')";
+                                        Statement statementcb = conn.createStatement();
+                                        statementcb.executeUpdate(sql);
+                                        sql = "UPDATE  mb_notifications SET mno_etat = 1 WHERE mno_idnotification = \"" + id + "\"";
+                                        statementcb.executeUpdate(sql);
+                                        statementcb.close();
+                                        conn.close();
+                                    } catch (SQLException ignored) {}
+                        },
+                                resultSet.getInt("mno_idnotification"),
+                                resultSet.getInt("mno_cible"));
+                    }
+                    statement.close();
+                    connection.close();
+                } catch (SQLException ignored) { }
+                Thread.sleep(SettingsSystem.timeOfConsultation);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                this.run();
+            }
+        }
+    }
+
+    private interface Callback {
+        void callback(int id, int cible, String date);
+    }
+
+    private void envoieNotifications(Callback notification, int id, int cible) {
+        String date = LocalDate.now().toString();
+        notification.callback(id, cible, date);
+    }
+
+}
---
+++ chef-orchestre/src/ThreadEcouteDataBase.java
@@ -0,0 +1,80 @@
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.*;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ThreadEcouteDataBase extends Thread {
+    /**
+     * @description: thread qui se charge d'écouter tous les changements efféctuer dans la base de données
+     * vous pouver le modifier.
+     * */
+
+    // constant pour ignorer qlq tables (voir thread initialisation système)!
+    final Map<String, Long> ignoredTables = new HashMap<>();
+
+    @Override
+    public void run() {
+        BinaryLogClient client
+                = new BinaryLogClient(SettingsSystem.hostname, SettingsSystem.port, SettingsSystem.username, SettingsSystem.password);
+        client.registerEventListener(event -> {
+            EventData data = event.getData();
+
+
+            if (data instanceof TableMapEventData) {
+                if (((TableMapEventData)data).getTable().equals("mb_notifications"))
+                    ignoredTables.put(((TableMapEventData)data).getTable(), ((TableMapEventData)data).getTableId());
+                if (((TableMapEventData)data).getTable().equals("mb_envoie"))
+                    ignoredTables.put(((TableMapEventData)data).getTable(), ((TableMapEventData)data).getTableId());
+            }
+
+            if (data instanceof WriteRowsEventData) {
+                WriteRowsEventData insert = (WriteRowsEventData) data;
+                try {
+                    if ((insert.getTableId() != ignoredTables.get("mb_notifications")) && (insert.getTableId() != ignoredTables.get("mb_envoie"))) {
+                        for (Object[] row : insert.getRows()) {
+                            new ThreadCreationNotification(new NotificationMobility(
+                                    "INSERTION",
+                                    "Insertion dans la base de données !",
+                                    0,
+                                    2))
+                                    .start();
+                        }
+                    }
+                } catch (NullPointerException ignored) {}
+            } else if (data instanceof UpdateRowsEventData) {
+                UpdateRowsEventData update = (UpdateRowsEventData) data;
+                try {
+                    if ((update.getTableId() != ignoredTables.get("mb_notifications")) && (update.getTableId() != ignoredTables.get("mb_envoie"))) {
+                        for (Map.Entry<Serializable[], Serializable[]> row : update.getRows()) {
+                            new ThreadCreationNotification(new NotificationMobility(
+                                    "MODIFICATION",
+                                    "Modification dans la base de données !",
+                                    0,
+                                    4))
+                                    .start();
+                        }
+                    }
+                } catch (NullPointerException ignored) {}
+            } else if (data instanceof DeleteRowsEventData) {
+                DeleteRowsEventData delete = (DeleteRowsEventData) data;
+                try {
+                    if ((delete.getTableId() != ignoredTables.get("mb_notifications")) && (delete.getTableId() != ignoredTables.get("mb_envoie"))) {
+                        for (Object[] row : delete.getRows()) {
+                            new ThreadCreationNotification(new NotificationMobility(
+                                    "SUPPRESSION",
+                                    "Suppression dans la base de données !",
+                                    2,
+                                    3))
+                                    .start();
+                        }
+                    }
+                } catch (NullPointerException ignored) {}
+            }
+        });
+        try {
+            client.connect();
+        } catch (Exception ignored) {}
+    }
+}
